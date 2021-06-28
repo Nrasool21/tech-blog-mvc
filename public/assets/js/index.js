521febbfeb0198fe12e2566bb69498025879dc0c
Nazia R <nazia.huma@gmail.com>
2021-06-28 03:41:11 +0100

    client side js for signup

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,8 +1,32 @@
+const handleSignupSubmit = async (event) => {
+  event.preventDefault();
 
-const handleSignupSubmit = () => {
-  //post request with username and password
-  // auth/sign-up
-  //on success window.location to /login
+  const username = $("#username").val();
+  const password = $("#password").val();
+  const confirmPassword = $("#confirmPassword").val();
+  if (password === confirmPassword) {
+    const requestBody = {
+      username: username,
+      password: password,
+    };
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      redirect: "follow",
+      body: JSON.stringify(requestBody),
+    };
+    const response = await fetch("/auth/sign-up", options);
+    if (response.status === 200) {
+      window.location.replace("/login");
+    } else {
+      console.log("Failed to Sign up");
+    }
+  } else {
+    console.log("Passwords do not match");
+  }
 };
 
 const handleLoginSubmit = async (event) => {
@@ -24,15 +48,14 @@ const handleLoginSubmit = async (event) => {
     redirect: "follow",
     body: JSON.stringify(requestBody),
   };
-console.log(requestBody)
+
   const response = await fetch("/auth/login", options);
 
   if (response.status === 200) {
-    window.location.replace("/dashboard")
+    window.location.replace("/dashboard");
   } else {
     console.log("Failed to login");
   }
-
 };
 
 const handleLogoutClick = () => {
@@ -62,3 +85,4 @@ const handlePostDelete = () => {
 console.log("client-side JS");
 
 $("#login-form").submit(handleLoginSubmit);
+$("#sign-up-form").submit(handleSignupSubmit);
