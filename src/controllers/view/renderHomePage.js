371588617c0f6dc80bebd2ebf703c334b698271e
Nazia R <nazia.huma@gmail.com>
2021-06-29 03:48:23 +0100

    public navbar and update login and sign up page

--- src/controllers/view/renderHomePage.js
+++ src/controllers/view/renderHomePage.js
@@ -2,7 +2,8 @@ const renderHomePage = (req, res) => {
   //get all posts
   //send send posts to handlebars
   //the array of post we get from model will feed in here
-  res.render("homepage");
+  const { isLoggedIn } = req.session;
+  res.render("homepage", { isLoggedIn });
 };
 
 module.exports = renderHomePage;
